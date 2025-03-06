73fbb13dc5922988538ccd221f892310696024a6
Parikshit Misra <98179308+parikshit-parspec@users.noreply.github.com>
2025-03-06 13:26:54 +0530

    new stuffs

--- {{cookiecutter.app_name}}/cmd/{{cookiecutter.app_name}}/main.go
+++ {{cookiecutter.app_name}}/cmd/{{cookiecutter.app_name}}/main.go
@@ -8,6 +8,20 @@ import (
 )
 
 func main() {
+	// Load config
+	config.AddDriver(json.Driver)
+	env := config.GetEnv("_ENV", "dev")
+	err := config.LoadFiles("config/" + env + ".json")
+	if err != nil {
+		fmt.Printf("error loading config: %v\n", err)
+		panic(err)
+	}
+	err = config.LoadFiles("config/secrets.json")
+	if err != nil {
+		fmt.Printf("error loading secrets: %v\n", err)
+		fmt.Println("if the task is running in ECS, the secrets file is not needed")
+	}
+	
 	e := echo.New()
 
 	// Middleware
